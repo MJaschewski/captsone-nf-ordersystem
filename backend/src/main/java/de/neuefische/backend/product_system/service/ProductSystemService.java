9fc268f496bbcbcec274c68f6b206863d462777e
mjaschew <dermijasch@gmx.de>
2023-06-09 15:06:29 +0200

    addProduct in ProductSystemService added

--- backend/src/main/java/de/neuefische/backend/product_system/service/ProductSystemService.java
+++ backend/src/main/java/de/neuefische/backend/product_system/service/ProductSystemService.java
@@ -1,5 +1,7 @@
 package de.neuefische.backend.product_system.service;
 
+import de.neuefische.backend.product_system.model.ProductBody;
+import de.neuefische.backend.product_system.model.ProductDTO;
 import de.neuefische.backend.product_system.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -8,4 +10,10 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class ProductSystemService {
     private final ProductRepository productRepository;
+    private final GenerateIdService generateIdService;
+
+    public ProductBody addProduct(ProductDTO productDTO) {
+        ProductBody newProduct = new ProductBody(generateIdService.generateUUID(),productDTO.getName(),productDTO.getPrice(), productDTO.getAccessLevel());
+        return productRepository.save(newProduct);
+    }
 }
