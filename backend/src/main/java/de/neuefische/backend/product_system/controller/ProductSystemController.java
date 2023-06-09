83b4aa947c9ad0d002858626ea72eedae8457c0b
mjaschew <dermijasch@gmx.de>
2023-06-09 14:43:20 +0200

    Add addProduct in ProductSystemController

--- backend/src/main/java/de/neuefische/backend/product_system/controller/ProductSystemController.java
+++ backend/src/main/java/de/neuefische/backend/product_system/controller/ProductSystemController.java
@@ -1,7 +1,11 @@
 package de.neuefische.backend.product_system.controller;
 
+import de.neuefische.backend.product_system.model.ProductBody;
+import de.neuefische.backend.product_system.model.ProductDTO;
 import de.neuefische.backend.product_system.service.ProductSystemService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +14,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/productSystem")
 public class ProductSystemController {
     private final ProductSystemService productSystemService;
+
+    @PostMapping
+    public ProductBody addProduct(@RequestBody ProductDTO productDTO){
+        return productSystemService.addProduct(productDTO);
+    }
 }
