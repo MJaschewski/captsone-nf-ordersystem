2561500a04b9750b51e1d8fb507e5a1a5d44373a
mjaschew <dermijasch@gmx.de>
2023-06-12 11:38:33 +0200

    Removed enum

---
+++ backend/src/test/java/de/neuefische/backend/productsystem/controller/ProductSystemControllerTest.java
@@ -0,0 +1,44 @@
+package de.neuefische.backend.productsystem.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class ProductSystemControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void when_addProductBodyProductDTO_returnProductBody() throws Exception {
+        //When & Then
+        mockMvc.perform(post("/api/productSystem")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(""" 
+                                {
+                                    "name":"test",
+                                    "price":1244.99,
+                                    "accessLevel":"All"
+                                }
+                                """))
+                .andExpect(status().isOk())
+                .andExpect(content().json("""
+                        {
+                            "name":"test",
+                            "price":1244.99,
+                            "accessLevel":"All"
+                        }
+                        """
+                ));
+
+    }
+
+}
