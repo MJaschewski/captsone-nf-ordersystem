d3165b2de598d2a9c45a22cb052b681b75b23cc9
mjaschew <dermijasch@gmx.de>
2023-06-12 12:45:10 +0200

    Fixed Merge problem

--- backend/src/test/java/de/neuefische/backend/productsystem/service/ProductSystemServiceTest.java
+++ backend/src/test/java/de/neuefische/backend/productsystem/service/ProductSystemServiceTest.java
@@ -52,44 +52,6 @@ class ProductSystemServiceTest {
 
     @Test
     void when_addProductBodyWithInputAccessLevelLead_then_returnProductDTOWithRightProperties() {
-    void when_addProductBodyWithInputAccessLevelAll_then_returnProductDTOWithRightProperties() {
-        //Given
-        ProductDTO testInputAll = new ProductDTO("testName", 1.00, "All");
-        String testIdAll = "testIdAll";
-        when(generateIdService.generateUUID()).thenReturn(testIdAll);
-        ProductBody expected = new ProductBody(testIdAll, testInputAll.getName(), testInputAll.getPrice(), testInputAll.getAccessLevel());
-        when(productRepository.save(any())).thenReturn(expected);
-        //When
-        ProductBody actual = productSystemService.addProductBody(testInputAll);
-        //Then
-        verify(generateIdService).generateUUID();
-        verify(productRepository).save(any());
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    void when_addProductBodyWithInputAccessLevelPurchase_then_returnProductDTOWithRightProperties() {
-        //Given
-        ProductDTO testInputPurchase = new ProductDTO("testName", 1.00, "Purchase");
-        String testIdPurchase = "testIdPurchase";
-        when(generateIdService.generateUUID()).thenReturn(testIdPurchase);
-        ProductBody expected = new ProductBody(testIdPurchase, testInputPurchase.getName(), testInputPurchase.getPrice(), testInputPurchase.getAccessLevel());
-        when(productRepository.save(any())).thenReturn(expected);
-        ProductDTO testInputLead = new ProductDTO("testName", 1.00, "Lead");
-        String testIdLead = "testIdLead";
-        when(generateIdService.generateUUID()).thenReturn(testIdLead);
-        ProductBody expected = new ProductBody(testIdLead, testInputLead.getName(), testInputLead.getPrice(), testInputLead.getAccessLevel());
-        when(productRepository.save(any())).thenReturn(expected);
-        //When
-        ProductBody actual = productSystemService.addProductBody(testInputPurchase);
-        //Then
-        verify(generateIdService).generateUUID();
-        verify(productRepository).save(any());
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    void when_addProductBodyWithInputAccessLevelLead_then_returnProductDTOWithRightProperties() {
         //Given
         ProductDTO testInputLead = new ProductDTO("testName", 1.00, "Lead");
         String testIdLead = "testIdLead";
@@ -97,7 +59,6 @@ class ProductSystemServiceTest {
         ProductBody expected = new ProductBody(testIdLead, testInputLead.getName(), testInputLead.getPrice(), testInputLead.getAccessLevel());
         when(productRepository.save(any())).thenReturn(expected);
         //When
-        ProductBody actual = productSystemService.addProductBody(testInputLead);
         ProductBody actual = productSystemService.addProductBody(testInputLead);
         //Then
         verify(generateIdService).generateUUID();
