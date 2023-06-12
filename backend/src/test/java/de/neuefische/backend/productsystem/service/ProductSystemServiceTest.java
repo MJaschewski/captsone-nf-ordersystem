6ee1eef2c070af57a52282c7b651b0d4d755fb87
mjaschew <dermijasch@gmx.de>
2023-06-12 12:36:26 +0200

    Tests added

--- backend/src/test/java/de/neuefische/backend/productsystem/service/ProductSystemServiceTest.java
+++ backend/src/test/java/de/neuefische/backend/productsystem/service/ProductSystemServiceTest.java
@@ -19,27 +19,59 @@ class ProductSystemServiceTest {
     private final ProductSystemService productSystemService = new ProductSystemService(productRepository,generateIdService);
 
     @Test
-    void when_addProductBodyWithInput_then_returnProductDTOWithRightProperties(){
+    void when_addProductBodyWithInputAccessLevelAll_then_returnProductDTOWithRightProperties() {
         //Given
-            ProductDTO testInput = new ProductDTO("testName", 1.00, "All");
-        String testId = "testId";
-            when(generateIdService.generateUUID()).thenReturn(testId);
-        ProductBody expected = new ProductBody(testId, testInput.getName(), testInput.getPrice(), testInput.getAccessLevel());
-            when(productRepository.save(any())).thenReturn(expected);
+        ProductDTO testInputAll = new ProductDTO("testName", 1.00, "All");
+        String testIdAll = "testIdAll";
+        when(generateIdService.generateUUID()).thenReturn(testIdAll);
+        ProductBody expected = new ProductBody(testIdAll, testInputAll.getName(), testInputAll.getPrice(), testInputAll.getAccessLevel());
+        when(productRepository.save(any())).thenReturn(expected);
         //When
-            ProductBody actual = productSystemService.addProductBody(testInput);
+        ProductBody actual = productSystemService.addProductBody(testInputAll);
         //Then
-            verify(generateIdService).generateUUID();
-            verify(productRepository).save(any());
-            assertEquals(expected,actual);
+        verify(generateIdService).generateUUID();
+        verify(productRepository).save(any());
+        assertEquals(expected, actual);
     }
 
     @Test
-    void when_addProductNegativePrice_then_returnIllegalArgumentExceptionWithMessage(){
+    void when_addProductBodyWithInputAccessLevelPurchase_then_returnProductDTOWithRightProperties() {
+        //Given
+        ProductDTO testInputPurchase = new ProductDTO("testName", 1.00, "Purchase");
+        String testIdPurchase = "testIdPurchase";
+        when(generateIdService.generateUUID()).thenReturn(testIdPurchase);
+        ProductBody expected = new ProductBody(testIdPurchase, testInputPurchase.getName(), testInputPurchase.getPrice(), testInputPurchase.getAccessLevel());
+        when(productRepository.save(any())).thenReturn(expected);
+        //When
+        ProductBody actual = productSystemService.addProductBody(testInputPurchase);
+        //Then
+        verify(generateIdService).generateUUID();
+        verify(productRepository).save(any());
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void when_addProductBodyWithInputAccessLevelLead_then_returnProductDTOWithRightProperties() {
+        //Given
+        ProductDTO testInputLead = new ProductDTO("testName", 1.00, "Lead");
+        String testIdLead = "testIdLead";
+        when(generateIdService.generateUUID()).thenReturn(testIdLead);
+        ProductBody expected = new ProductBody(testIdLead, testInputLead.getName(), testInputLead.getPrice(), testInputLead.getAccessLevel());
+        when(productRepository.save(any())).thenReturn(expected);
+        //When
+        ProductBody actual = productSystemService.addProductBody(testInputLead);
+        //Then
+        verify(generateIdService).generateUUID();
+        verify(productRepository).save(any());
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void when_addProductNegativePrice_then_returnIllegalArgumentExceptionWithMessage() {
         //Given
         ProductDTO wrongInput = new ProductDTO("testName", -3.00, "All");
         //When & Then
-            assertThrows(IllegalArgumentException.class
-                    , ()->productSystemService.addProductBody(wrongInput),"Price can't be negative");
+        assertThrows(IllegalArgumentException.class
+                , () -> productSystemService.addProductBody(wrongInput), "Price can't be negative");
     }
 }
