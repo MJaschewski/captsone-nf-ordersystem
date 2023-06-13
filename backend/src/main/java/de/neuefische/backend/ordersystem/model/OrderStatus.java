2a0e59bd9f35dd06eea74fb06a5bc0bd40dce541
mjaschew <dermijasch@gmx.de>
2023-06-13 17:07:22 +0200

    OrderStatus simplified

--- backend/src/main/java/de/neuefische/backend/ordersystem/model/OrderStatus.java
+++ backend/src/main/java/de/neuefische/backend/ordersystem/model/OrderStatus.java
@@ -1,12 +1,9 @@
 package de.neuefische.backend.ordersystem.model;
 
 public enum OrderStatus {
-    REQUESTED("Requested"),
-    IN_TRANSIT("In_Transit"),
-    ARRIVED("Arrived"),
-    CANCELLED("Cancelled");
-
-    OrderStatus(String status) {
-    }
+    REQUESTED,
+    IN_TRANSIT,
+    ARRIVED,
+    CANCELLED;
 }
 
