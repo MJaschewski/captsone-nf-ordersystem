f7516ee792e7dad00f8007a4184a8ef564a8fdbe
mjaschew <dermijasch@gmx.de>
2023-06-13 16:59:52 +0200

    Add Order Frontend added

--- frontend/src/components/all/AddOrder.tsx
+++ frontend/src/components/all/AddOrder.tsx
@@ -1,16 +1,20 @@
-import React, {useEffect, useState} from 'react';
+import React, { FormEvent, useEffect, useState} from 'react';
 import {ProductBodyType} from "../purchase/ProductBodyType";
 import axios from "axios";
-import ProductOpticalElement from "../purchase/ProductOpticalElement";
 import {NavigateFunction} from "react-router-dom";
 
 type Props = {
     navigate: NavigateFunction
 }
 
+type OrderDTO = {
+    productBodyList:ProductBodyType[]
+}
+
 function AddOrder(props: Props) {
 
-    const [productList, setProductList] = useState<ProductBodyType[]>([])
+    const [productList,setProductList] = useState<ProductBodyType[]>([])
+    const [validProductList, setValidProductList] = useState<ProductBodyType[]>([])
 
     useEffect(handleProductList, [])
 
@@ -18,20 +22,52 @@ function AddOrder(props: Props) {
         axios.get('/api/productSystem')
             .then(response => response.data)
             .then(data => {
-                setProductList(data);
+                setValidProductList(data);
                 console.log(data)
             })
             .catch(error => console.log(error))
     }
 
+    function handleAddProduct(newProduct:ProductBodyType){
+         const index = productList.indexOf(newProduct);
+        if (index > -1) {
+            productList.splice(index, 1)
+        } else {
+            let newProductList = productList;
+            newProductList.push(newProduct);
+            setProductList(newProductList);
+        }
+    }
+
+    function handleSubmit (event: FormEvent){
+        event.preventDefault()
+        const orderDTO:OrderDTO = {productBodyList:productList}
+        axios.post('/api/orderSystem',orderDTO)
+            .then(response => console.log(response.data))
+            .then(()=>props.navigate("/"))
+            .catch(error => console.log(error))
+    }
+
+
     return (
+
         <div>
             <h1>Add Order</h1>
             <h2>List of Products:</h2>
-            {productList.map(currentProductBody => {
-                return <ProductOpticalElement key={currentProductBody.id} productBody={currentProductBody}/>
-            })}
-            <form>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    {validProductList.map( (currentProduct) => (
+                        <div key={currentProduct.name}>
+                            <input type="checkbox"
+                                   id={currentProduct.id}
+                                   name="ProductOption"
+                                   value={currentProduct.name}
+                                   onClick={()=>handleAddProduct(currentProduct)}
+                            />
+                            <label>{currentProduct.name}</label>
+                        </div>
+                    ) )}
+                </label>
                 <button>Add Order</button>
             </form>
             <button onClick={() => props.navigate("/")}>Cancel</button>
