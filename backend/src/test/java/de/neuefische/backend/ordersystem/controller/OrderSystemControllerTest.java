c46afba7904545b62034461695309ad774ba7288
mjaschew <dermijasch@gmx.de>
2023-06-14 16:46:40 +0200

    Integrationtest added for editOrderById

--- backend/src/test/java/de/neuefische/backend/ordersystem/controller/OrderSystemControllerTest.java
+++ backend/src/test/java/de/neuefische/backend/ordersystem/controller/OrderSystemControllerTest.java
@@ -1,6 +1,5 @@
 package de.neuefische.backend.ordersystem.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import de.neuefische.backend.ordersystem.model.OrderBody;
 import de.neuefische.backend.productsystem.model.ProductBody;
@@ -14,10 +13,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import java.io.UnsupportedEncodingException;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
@@ -298,7 +294,153 @@ class OrderSystemControllerTest {
         //Given
         String wrongId = "wrongId";
         //When & Then
-        mockMvc.perform(get("/api/orderSystem/"+wrongId))
+        mockMvc.perform(get("/api/orderSystem/" + wrongId))
                 .andExpect(status().isNotFound());
     }
+
+    @Test
+    void when_editOrderByIdWrongId_then_return404() throws Exception {
+        //Given
+        String wrongId = "wrongId";
+        //When & Then
+        mockMvc.perform(put("/api/orderSystem/" + wrongId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                                    {
+                                    "productBodyList":[
+                                        {
+                                            "id": "wrongId",
+                                            "name": "testWrong",
+                                            "price": 1244.99,
+                                            "accessLevel": "wrongLevel"
+                                        }
+                                        ]
+                                    }
+                                """))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void when_editOrderById_then_return200OkAndChangedOrder() throws Exception {
+        //Given
+        MvcResult postProduct = mockMvc.perform(post("/api/productSystem")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(""" 
+                                {
+                                    "name":"test",
+                                    "price":1244.99,
+                                    "accessLevel":"ALL"
+                                }
+                                """))
+                .andExpect(status().isOk())
+                .andExpect(content().json("""
+                        {
+                            "name":"test",
+                            "price":1244.99,
+                            "accessLevel":"ALL"
+                        }
+                        """
+                )).andReturn();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        ProductBody newProduct = objectMapper.readValue(postProduct.getResponse().getContentAsString(), ProductBody.class);
+
+        MvcResult orderResult = mockMvc.perform(MockMvcRequestBuilders.post("/api/orderSystem")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                                    {
+                                    "productBodyList":[
+                                        {
+                                            "id": "%s",
+                                            "name": "%s",
+                                            "price": 1244.99,
+                                            "accessLevel": "%s"
+                                        }
+                                        ]
+                                    }
+                                """.formatted(newProduct.getId(), newProduct.getName(), newProduct.getAccessLevel())
+                        ))
+                .andExpect(status().isOk())
+                .andExpect(content().json("""
+                        {
+                            "productBodyList": [
+                                {
+                                    "id": "%s",
+                                    "name": "%s",
+                                    "price": 1244.99,
+                                    "accessLevel": "%s"
+                                }
+                            ],
+                            "price": 1244.99,
+                            "created": "%s",
+                            "arrival": "No date yet",
+                            "approvalPurchase": false,
+                            "approvalLead": false,
+                            "orderStatus": "REQUESTED"
+                        }
+                        """.formatted(newProduct.getId(), newProduct.getName(), newProduct.getAccessLevel(), timeService.currentDate())))
+                .andExpect(jsonPath("$.productBodyList[0].id").isNotEmpty()).andReturn();
+
+        OrderBody addedOrder = objectMapper.readValue(orderResult.getResponse().getContentAsString(), OrderBody.class);
+
+        //When&Then
+        mockMvc.perform(put("/api/orderSystem/" + addedOrder.getId())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                                    {
+                                    "productBodyList":[
+                                        {
+                                            "id": "%s",
+                                            "name": "%s",
+                                            "price": 1244.99,
+                                            "accessLevel": "%s"
+                                        },
+                                        {
+                                            "id": "%s",
+                                            "name": "%s",
+                                            "price": 1244.99,
+                                            "accessLevel": "%s"
+                                        }
+                                        ]
+                                    }
+                                """.formatted(newProduct.getId(), newProduct.getName(), newProduct.getAccessLevel()
+                                , newProduct.getId(), newProduct.getName(), newProduct.getAccessLevel())
+                        ))
+                .andExpect(status().isOk())
+                .andExpect(content().json("""
+                        {
+                            "id":"%s",
+                            "productBodyList": [
+                                {
+                                    "id": "%s",
+                                    "name": "%s",
+                                    "price": 1244.99,
+                                    "accessLevel": "%s"
+                                },
+                                {
+                                    "id": "%s",
+                                    "name": "%s",
+                                    "price": 1244.99,
+                                    "accessLevel": "%s"
+                                }
+                            ],
+                            "price": 1244.99,
+                            "created": "%s",
+                            "arrival": "No date yet",
+                            "approvalPurchase": false,
+                            "approvalLead": false,
+                            "orderStatus": "REQUESTED"
+                        }
+                        """.formatted(
+                        addedOrder.getId()
+                        , addedOrder.getProductBodyList().get(0).getId()
+                        , addedOrder.getProductBodyList().get(0).getName()
+                        , addedOrder.getProductBodyList().get(0).getAccessLevel()
+                        , addedOrder.getProductBodyList().get(0).getId()
+                        , addedOrder.getProductBodyList().get(0).getName()
+                        , addedOrder.getProductBodyList().get(0).getAccessLevel()
+                        , timeService.currentDate())));
+
+    }
+
 }
