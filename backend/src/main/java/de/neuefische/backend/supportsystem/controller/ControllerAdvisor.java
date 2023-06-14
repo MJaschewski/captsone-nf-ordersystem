cde2335556a81120df2b58f77283ef1fb48f272c
mjaschew <dermijasch@gmx.de>
2023-06-14 15:11:40 +0200

    IntegrationTest added

--- backend/src/main/java/de/neuefische/backend/supportsystem/controller/ControllerAdvisor.java
+++ backend/src/main/java/de/neuefische/backend/supportsystem/controller/ControllerAdvisor.java
@@ -5,11 +5,18 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.NoSuchElementException;
+
 @ControllerAdvice
 public class ControllerAdvisor {
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<Object> handleInputMismatchException(IllegalArgumentException illegalArgumentException) {
         return new ResponseEntity<>(illegalArgumentException.getMessage(), HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<Object> handleInputMismatchException(NoSuchElementException noSuchElementException) {
+        return new ResponseEntity<>(noSuchElementException.getMessage(), HttpStatus.NOT_FOUND);
     }
 }
