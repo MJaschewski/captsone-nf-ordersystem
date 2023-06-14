af2e0db7945ed5919bea3d035ff5c2827b634b1e
mjaschew <dermijasch@gmx.de>
2023-06-14 12:28:00 +0200

    NoSuchElementException in Advisor to 404 added

--- backend/src/main/java/de/neuefische/backend/supportsystem/controller/ControllerAdvisor.java
+++ backend/src/main/java/de/neuefische/backend/supportsystem/controller/ControllerAdvisor.java
@@ -5,11 +5,18 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.NoSuchElementException;
+
 @ControllerAdvice
 public class ControllerAdvisor {
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<Object> handleInputMismatchException(IllegalArgumentException illegalArgumentException) {
         return new ResponseEntity<>(illegalArgumentException.getMessage(), HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<Object> handleInputMismatchException(NoSuchElementException moSuchElementException) {
+        return new ResponseEntity<>(moSuchElementException.getMessage(), HttpStatus.NOT_FOUND);
     }
 }
