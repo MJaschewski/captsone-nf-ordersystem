39663a6cd185213d56790ba7211375125f59202b
mjaschew <dermijasch@gmx.de>
2023-06-19 09:45:01 +0200

    UserControllerTest added

--- backend/src/main/java/de/neuefische/backend/usersystem/controller/UserController.java
+++ backend/src/main/java/de/neuefische/backend/usersystem/controller/UserController.java
@@ -3,12 +3,9 @@ package de.neuefische.backend.usersystem.controller;
 import de.neuefische.backend.usersystem.service.UserSystemService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.security.Principal;
 
 @RequiredArgsConstructor
 @RequestMapping("api/userSystem")
@@ -16,23 +13,6 @@ import java.security.Principal;
 public class UserController {
 
     private final UserSystemService userSystemService;
-
-    @GetMapping("/me")
-    public String getMeControllerOnly(Principal principal) {
-        if (principal != null) {
-            return principal.getName();
-        } else {
-            return "Anonymous User.";
-        }
-    }
-
-    @GetMapping("/me2")
-    public String getMeFromEverywhere() {
-        return SecurityContextHolder
-                .getContext()
-                .getAuthentication()
-                .getName();
-    }
 
     @PostMapping("/login")
     public String login() {
