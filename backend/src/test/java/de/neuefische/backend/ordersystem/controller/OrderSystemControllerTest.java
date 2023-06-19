abc17a62df00160f13509a27a288b221f48af4a4
mjaschew <dermijasch@gmx.de>
2023-06-19 16:05:29 +0200

    Integration Tests for deleteOrder added

--- backend/src/test/java/de/neuefische/backend/ordersystem/controller/OrderSystemControllerTest.java
+++ backend/src/test/java/de/neuefische/backend/ordersystem/controller/OrderSystemControllerTest.java
@@ -473,4 +473,104 @@ class OrderSystemControllerTest {
 
     }
 
+    @Test
+    @DirtiesContext
+    @WithMockUser
+    void when_deleteOrderByIdWrongId_throw404() throws Exception {
+        //Given
+        String wrongId = "wrongId";
+        //When & Then
+        mockMvc.perform(delete("/api/orderSystem/" + wrongId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                                    {
+                                    "productBodyList":[
+                                        {
+                                            "id": "wrongId",
+                                            "name": "testWrong",
+                                            "price": 1244.99,
+                                            "accessLevel": "wrongLevel"
+                                        }
+                                        ]
+                                    }
+                                """)
+                        .with(csrf()))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    @DirtiesContext
+    @WithMockUser
+    void when_deleteOrderById_then_return200OkAndMessage() throws Exception {
+        //Given
+        MvcResult postProduct = mockMvc.perform(post("/api/productSystem")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(""" 
+                                {
+                                    "name":"test",
+                                    "price":1244.99,
+                                    "accessLevel":"ALL"
+                                }
+                                """)
+                        .with(csrf()))
+                .andExpect(status().isOk())
+                .andExpect(content().json("""
+                        {
+                            "name":"test",
+                            "price":1244.99,
+                            "accessLevel":"ALL"
+                        }
+                        """
+                )).andReturn();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        ProductBody newProduct = objectMapper.readValue(postProduct.getResponse().getContentAsString(), ProductBody.class);
+
+        MvcResult orderResult = mockMvc.perform(MockMvcRequestBuilders.post("/api/orderSystem")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                                    {
+                                    "productBodyList":[
+                                        {
+                                            "id": "%s",
+                                            "name": "%s",
+                                            "price": 1244.99,
+                                            "accessLevel": "%s"
+                                        }
+                                        ]
+                                    }
+                                """.formatted(newProduct.getId(), newProduct.getName(), newProduct.getAccessLevel())
+                        )
+                        .with(csrf()))
+                .andExpect(status().isOk())
+                .andExpect(content().json("""
+                        {
+                            "productBodyList": [
+                                {
+                                    "id": "%s",
+                                    "name": "%s",
+                                    "price": 1244.99,
+                                    "accessLevel": "%s"
+                                }
+                            ],
+                            "price": 1244.99,
+                            "created": "%s",
+                            "arrival": "No date yet",
+                            "approvalPurchase": false,
+                            "approvalLead": false,
+                            "orderStatus": "REQUESTED"
+                        }
+                        """.formatted(newProduct.getId(), newProduct.getName(), newProduct.getAccessLevel(), timeService.currentDate())))
+                .andExpect(jsonPath("$.productBodyList[0].id").isNotEmpty()).andReturn();
+
+        OrderBody addedOrder = objectMapper.readValue(orderResult.getResponse().getContentAsString(), OrderBody.class);
+
+        //When&Then
+        mockMvc.perform(delete("/api/orderSystem/" + addedOrder.getId())
+                        .with(csrf()))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Deletion successful"));
+
+    }
+
 }
