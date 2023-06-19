6262c109d85c92be34cb50e27ef29b99987bd95f
mjaschew <dermijasch@gmx.de>
2023-06-19 16:23:10 +0200

    Frontend deleteOrder

---
+++ frontend/src/components/all/OrderSystem/DeleteOrder.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {useNavigate, useParams} from "react-router-dom";
+
+import axios from "axios";
+
+function DeleteOrder() {
+    const navigate = useNavigate();
+    let {id} = useParams();
+
+    function handleDeletion() {
+        axios.delete('/api/orderSystem/' + id)
+            .then(response => console.log(response.data))
+            .then(() => navigate("/orderHub"))
+            .catch(error => console.log(error))
+    }
+
+    return (
+        <div>
+            <h2>Do you really want to delete order: {id}?</h2>
+            <button onClick={handleDeletion}>Yes</button>
+            <button onClick={() => navigate("/orderHub/details/" + id)}> No</button>
+        </div>
+    );
+}
+
+export default DeleteOrder;
