afdfb6ebf9608e92d21e8c18ed04841b340a7506
mjaschew <dermijasch@gmx.de>
2023-06-27 13:19:25 +0200

    OrderHub.css added

--- frontend/src/components/all/OrderSystem/OrderHub.tsx
+++ frontend/src/components/all/OrderSystem/OrderHub.tsx
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from 'react';
 import {useNavigate} from "react-router-dom";
 import {OrderBodyType} from "./OrderBodyType";
 import axios from "axios";
+import "./styles/OrderHub.css"
 
 function OrderHub() {
     const navigate = useNavigate();
@@ -20,17 +21,17 @@ function OrderHub() {
     }
 
     return (
-        <div>
+        <div className="OrderHub">
             <h1>Manage Your Orders</h1>
             <h2>List of Orders:</h2>
-            <ul>{orderList.map( currentOrderBody => (
-                    <li key={currentOrderBody.id}>
-                        <p>OrderId: {currentOrderBody.id}</p>
-                        <p>Created: {currentOrderBody.created}</p>
-                        <p>Status: {currentOrderBody.orderStatus}</p>
-                        <p>
-                            <button onClick={() => navigate("/orderHub/details/" + currentOrderBody.id)}>Details
-                            </button>
+            <ul>{orderList.map(currentOrderBody => (
+                <li key={currentOrderBody.id}>
+                    <p>OrderId: {currentOrderBody.id}</p>
+                    <p>Created: {currentOrderBody.created}</p>
+                    <p>Status: {currentOrderBody.orderStatus}</p>
+                    <p>
+                        <button onClick={() => navigate("/orderHub/details/" + currentOrderBody.id)}>Details
+                        </button>
                         </p>
                     </li>
                 )
