43f14ae30a7b06892416a5a7b1d6b4c8afc6e13e
mjaschew <dermijasch@gmx.de>
2023-06-29 16:48:03 +0200

    fix SecurityConfig

--- backend/src/main/java/de/neuefische/backend/usersystem/security/SecurityConfig.java
+++ backend/src/main/java/de/neuefische/backend/usersystem/security/SecurityConfig.java
@@ -55,7 +55,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.GET, "api/productSystem").hasAuthority(ROLE_ALL)
                         .requestMatchers(HttpMethod.POST, "api/productSystem").hasAuthority(ROLE_PURCHASE)
                         .requestMatchers("api/productSystem/**").hasAuthority(ROLE_PURCHASE)
-                        .anyRequest().permitAll());
+                        .anyRequest().denyAll());
         return httpSecurity.build();
     }
 }
