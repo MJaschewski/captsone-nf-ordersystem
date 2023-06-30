6d06d9c519465e204971e11c73f403bdea811f97
mjaschew <dermijasch@gmx.de>
2023-06-30 12:08:33 +0200

    fix imports in UserHub

--- frontend/src/components/lead/UserHub.tsx
+++ frontend/src/components/lead/UserHub.tsx
@@ -1,8 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import {useNavigate} from "react-router-dom";
 import axios from "axios";
-import {Simulate} from "react-dom/test-utils";
-
 
 type UserSimpleBody = {
     username: string
