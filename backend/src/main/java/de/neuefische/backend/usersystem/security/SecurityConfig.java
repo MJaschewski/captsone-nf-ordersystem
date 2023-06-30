4352298941412034435f022cd9c567fd92f25f56
mjaschew <dermijasch@gmx.de>
2023-06-30 11:38:39 +0200

    Security updated and integrationTests

--- backend/src/main/java/de/neuefische/backend/usersystem/security/SecurityConfig.java
+++ backend/src/main/java/de/neuefische/backend/usersystem/security/SecurityConfig.java
@@ -46,6 +46,7 @@ public class SecurityConfig {
                         .requestMatchers("api/userSystem/login").authenticated()
                         .requestMatchers("api/userSystem/logout").authenticated()
                         .requestMatchers("api/userSystem/register").hasAuthority(ROLE_LEAD)
+                        .requestMatchers("api/userSystem/users").hasAuthority(ROLE_LEAD)
                         .requestMatchers("api/orderSystem/approve/**").hasAnyAuthority(ROLE_PURCHASE, ROLE_LEAD)
                         .requestMatchers("api/orderSystem/disapprove/**").hasAnyAuthority(ROLE_PURCHASE, ROLE_LEAD)
                         .requestMatchers("api/orderSystem/own").hasAuthority(ROLE_ALL)
