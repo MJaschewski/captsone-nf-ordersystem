9a5f6b882295a6fc5cf53f0af7bacbbdc036d4cd
mjaschew <dermijasch@gmx.de>
2023-07-04 14:24:37 +0200

    Change Password and styling cancel buttons

--- frontend/src/components/all/AccountPage.tsx
+++ frontend/src/components/all/AccountPage.tsx
@@ -1,13 +1,22 @@
-import React, {useEffect, useState} from 'react';
+import React, {ChangeEvent, FormEvent, useEffect, useState} from 'react';
 import {useNavigate} from "react-router-dom";
 import {UserSimpleBody} from "../UserSimpleBodyType";
 import secureLocalStorage from "react-secure-storage";
+import axios from "axios";
+
+type PasswordChangeDTO = {
+    oldPassword: string,
+    newPassword: string
+}
 
 function AccountPage() {
     const navigate = useNavigate();
     const [ownUser, setOwnUser] = useState<UserSimpleBody>({username: "Anonymous User", authorities: []});
     const [showPasswordChange, setShowPasswordChange] = useState(false);
+    const [oldPassword, setOldPassword] = useState<string>("");
+    const [newPassword, setNewPassword] = useState<string>("");
 
+    // eslint-disable-next-line
     useEffect(handleSetUser, [])
 
     function handleSetUser() {
@@ -16,6 +25,24 @@ function AccountPage() {
         typeof userName === "string"
             ? setOwnUser({username: userName, authorities: authorities})
             : navigate("/login")
+
+    }
+
+    function handleOldPasswordChange(event: ChangeEvent<HTMLInputElement>) {
+        setOldPassword(event.target.value)
+    }
+
+    function handleNewPasswordChange(event: ChangeEvent<HTMLInputElement>) {
+        setNewPassword(event.target.value)
+    }
+
+    function handleChangePasswordSubmit(event: FormEvent) {
+        event.preventDefault();
+        const passwordChangeDTO: PasswordChangeDTO = {oldPassword: oldPassword, newPassword: newPassword}
+        axios.put('/api/userSystem/password', passwordChangeDTO)
+            .then(response => console.log(response.data))
+            .then(() => setShowPasswordChange(false))
+            .catch(error => console.log(error));
 
     }
 
@@ -28,10 +55,23 @@ function AccountPage() {
             {!showPasswordChange
                 ? <button onClick={() => setShowPasswordChange(true)}>Change Password?</button>
                 : <div className="passwordChange-Wrapper">
-                    <form>
+                    <h3>Password Change:</h3>
+                    <form onSubmit={handleChangePasswordSubmit}>
+                        <label htmlFor="oldPassword">
+                            Old Password:
+                            <input type="password" name="oldPassword" value={oldPassword}
+                                   onChange={handleOldPasswordChange}/>
+                        </label>
+                        <label htmlFor="newPassword">
+                            New Password:
+                            <input type="password" name="newPassword" value={newPassword}
+                                   onChange={handleNewPasswordChange}/>
+                        </label>
+
                         <button className="button-submit-wrapper">Change Password</button>
                     </form>
-                    <button onClick={() => setShowPasswordChange(false)}>Cancel</button>
+                    <button className="button-cancel-wrapper" onClick={() => setShowPasswordChange(false)}>Cancel
+                    </button>
                 </div>
 
             }
